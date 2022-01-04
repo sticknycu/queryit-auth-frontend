fb44ef35c04ad1f25c1cc39090930e3c46962cac
nicugnm <nicolae.ghergu@s.unibuc.ro>
2022-01-04 14:05:01 +0200

    Add redirect after login to dashboard page.

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Inject, OnInit} from '@angular/core';
+import { DOCUMENT } from "@angular/common";
 
 @Component({
   selector: 'app-login',
@@ -8,7 +9,12 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
   focus;
   focus1;
-  constructor() { }
+  constructor(@Inject(DOCUMENT) private document: Document) { }
+
+  public handleLogin() {
+    // redirect to dashboardD
+    this.document.location.href="http://localhost:60028/";
+  }
 
   ngOnInit() {
   }
